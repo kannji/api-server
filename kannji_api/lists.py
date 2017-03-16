0662b2d4905c05f310031a500239b82e4ee12e2a
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-03-16 19:31:01 +0100

    - added routes and implementation for querying lists from the server

--- kannji_api/lists.py
+++ kannji_api/lists.py
@@ -8,22 +8,45 @@ from kannji_api.list_model import Lists
 def get_list(response, list_id):
 
     # get list
-    list = get_object_or_404(Lists, list_id=list_id)
+    learning_list = get_object_or_404(Lists, list_id=list_id)
 
-    responseJson = {}
-    responseJson['list_id'] = list.list_id
-    responseJson['name'] = list.name
-    responseJson['description'] = list.description
-    responseJson['thumbnail'] = list.thumbnail
-    responseJson['entries'] = {}
+    return JsonResponse(get_learning_list_object(learning_list))
+
+
+def get_all_lists_brief(response):
+
+    # get list
+    learning_lists = Lists.objects.all()
+
+    learning_lists_object = {}
+
+    for learning_list in learning_lists:
+        learning_lists_object[learning_list.list_id] = get_learning_list_brief_object(learning_list)
+
+    return JsonResponse(learning_lists_object)
+
+
+def get_learning_list_brief_object(learning_list):
+    learningListBriefObject = {}
+    learningListBriefObject['list_id'] = learning_list.list_id
+    learningListBriefObject['name'] = learning_list.name
+    learningListBriefObject['description'] = learning_list.description
+    learningListBriefObject['thumbnail_url'] = learning_list.thumbnail_url
+
+    return learningListBriefObject
+
+
+def get_learning_list_object(learning_list):
+    learningListObject = get_learning_list_brief_object(learning_list)
+    learningListObject['entries'] = {}
 
     # add kanji to the list
     # TODO what to do with big lists?
-    kanji = list.kanji.all()
+    kanji = learning_list.kanji.all()
 
     for current_kanji in kanji:
         kanjiObject = get_kanji_object(current_kanji)
         kanjiObject['type'] = 'kanji'
-        responseJson['entries'][current_kanji.kanji_id] = kanjiObject
+        learningListObject['entries'][current_kanji.kanji_id] = kanjiObject
 
-    return JsonResponse(responseJson)
+    return learningListObject
--- kannji_api/urls.py
+++ kannji_api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', kanji.index, name='index'),
     url(r'^lists/$', kanji.index, name='index'),
     url(r'^lists/(?P<list_id>[0-9]+)/$', lists.get_list, name='index'),
+    url(r'^lists/all/$', lists.get_all_lists_brief, name='index'),
     url(r'^kanji/$', kanji.get_all_kanji, name='get_all_kanji'),
     url(r'^kanji/(?P<kanji_id>[0-9]+)/$', kanji.get_kanji, name='get_kanji'),
     url(r'^kanji/random/$', kanji.get_random_kanji, name='get_random_kanji'),
