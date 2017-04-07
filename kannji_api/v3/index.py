9250a1c1f52d69e99bb2d4d591e714c4a3cdafe9
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-04-07 16:36:03 +0200

    - added user profile api

--- kannji_api/v3/index.py
+++ kannji_api/v3/index.py
@@ -3,8 +3,3 @@ from django.http.response import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, you are at the Kannji api version 3 index.")
-
-# protected content, can only be accessed when logged in
-@login_required()
-def secret_page(request, *args, **kwargs):
-    return HttpResponse('Secret contents!', status=200)
--- kannji_api/v3/urls.py
+++ kannji_api/v3/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url, include
 import oauth2_provider.views as oauth2_views
 
-from kannji_api.v3 import index as index_v3
+from kannji_api.v3 import index as index_v3, user as user_v3
 
 urlpatterns = [
     # changed part of the api
     url(r'^oauth2/token/$', oauth2_views.TokenView.as_view(), name="token"),
     url(r'^oauth2/revoke-token/$', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
     url(r'^$', index_v3.index, name='index'),
-    url(r'^secret/$', index_v3.secret_page, name='index'),
+    url(r'^user/$', user_v3.user_profile, name='index'),
 
     # unchanged part
     url(r'^', include('kannji_api.v2.urls')),
