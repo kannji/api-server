1f1db59cc501f70c8eb60efd78c51f18783e2a0e
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-08-24 13:46:57 +0200

    split models.py into separate files for better overview

--- kannji_api/models/kana.py
+++ kannji_api/models/kana.py
@@ -57,20 +57,21 @@ class Kana(models.Model):
 	
 	def __str__(self):
 		return self.literal
-
+	
 	class Meta:
 		abstract = True
+
 
 class Hiragana(Kana):
 	# the id of the corresponding Monograph
 	# e.g. for the diacritic gi (ぎ), the digraph kyo (きょ) and the diacritic-digraph (ぎょ)
 	# this will hold the uuid of the monograph ki (き)
 	monograph = models.ForeignKey('self', related_name='monograph_uuid')
-
+	
 	# the id of the corresponding Diacritic
 	# e.g. for the diacritic-digraph gyo (ぎょ) this will hold the uuid of the diacritic ki (ぎ)
 	diacritic = models.ForeignKey('self', related_name='diacritic_uuid')
-
+	
 	# the id of the corresponding Digraph
 	# e.g. for the diacritic-digraph gyo (ぎょ) this will hold the uuid of the digraph ki (きょ)
 	digraph = models.ForeignKey('self', related_name='digraph_uuid')
@@ -83,72 +84,13 @@ class Katakana(Kana):
 	# e.g. for the diacritic gi (ギ), the digraph kyo (キョ) and the diacritic-digraph (ギョ)
 	# this will hold the uuid of the monograph ki (キ)
 	monograph = models.ForeignKey('self', related_name='monograph_uuid')
-
+	
 	# the id of the corresponding Diacritic
 	# e.g. for the diacritic-digraph gyo (ギョ) this will hold the uuid of the diacritic ki (ギ)
 	diacritic = models.ForeignKey('self', related_name='diacritic_uuid')
-
+	
 	# the id of the corresponding Digraph
 	# e.g. for the diacritic-digraph gyo (ギョ) this will hold the uuid of the digraph ki (キョ)
 	digraph = models.ForeignKey('self', related_name='digraph_uuid')
 	
 	corresponding_hiragana = models.ForeignKey('kannji_api.Hiragana')
-
-
-class Kanji(models.Model):
-	kanji_id = models.BigAutoField(primary_key=True)
-	literal = models.CharField(max_length=1)
-	stroke_count = models.PositiveSmallIntegerField()
-	jlpt_level = models.PositiveSmallIntegerField(null=True)
-	school_grade = models.PositiveSmallIntegerField(null=True)
-	frequency = models.PositiveSmallIntegerField(null=True)
-	
-	def __str__(self):
-		return self.literal
-
-
-class KanjiMeanings(models.Model):
-	meaning_id = models.BigAutoField(primary_key=True)
-	kanji = models.ForeignKey(Kanji, related_name='kanji_meanings_set', on_delete=models.CASCADE)
-	meaning = models.CharField(max_length=200, null=True)
-	language = models.CharField(max_length=7, null=True)
-	
-	class Meta:
-		unique_together = (('meaning_id', 'kanji'), ('kanji', 'meaning', 'language'),)
-	
-	def __str__(self):
-		return self.meaning
-
-
-class KanjiReadings(models.Model):
-	reading_id = models.BigAutoField(primary_key=True)
-	kanji = models.ForeignKey(Kanji, related_name='kanji_readings_set', on_delete=models.CASCADE)
-	reading = models.CharField(max_length=20)
-	type = models.CharField(max_length=7)
-	
-	class Meta:
-		unique_together = (('reading_id', 'kanji'), ('kanji', 'reading', 'type'),)
-	
-	def __str__(self):
-		return self.reading
-
-
-class KanjiRadicals(models.Model):
-	radical_id = models.BigAutoField(primary_key=True)
-	kanji = models.ForeignKey(Kanji, related_name='kanji_radicals_set', on_delete=models.CASCADE)
-	radical = models.PositiveSmallIntegerField()
-	type = models.CharField(max_length=20)
-	
-	class Meta:
-		unique_together = (('radical_id', 'kanji'), ('kanji', 'radical', 'type'),)
-	
-	def __str__(self):
-		return self.radical
-
-
-class LearningLists(models.Model):
-	list_id = models.BigAutoField(primary_key=True)
-	kanji = models.ManyToManyField(Kanji)
-	name = models.CharField(max_length=200)
-	description = models.CharField(max_length=2000, null=True)
-	thumbnail_url = models.CharField(max_length=300, null=True)
