16c260d9a0c1601ce9431ce32eb3f531f4f4ba25
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-08-24 13:47:19 +0200

    made Kanji modle accesible in the admin backend

--- kannji_api/admin.py
+++ kannji_api/admin.py
@@ -1,6 +1,7 @@
-from kannji_api.models import Kana, Hiragana, Katakana
+from kannji_api.models import Hiragana, Katakana, Kanji
 from django.contrib import admin
 
 
 admin.site.register(Hiragana)
 admin.site.register(Katakana)
+admin.site.register(Kanji)
